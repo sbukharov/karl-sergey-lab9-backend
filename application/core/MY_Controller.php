a15faf967e2b8da7d8d9196349411e9bba261c95
sbukharov <sergey.bukharov@cluberudite.com>
2017-12-03 14:05:17 -0800

    Removed unused backend features.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -32,7 +32,6 @@ class Application extends CI_Controller
 	 */
 	 function render($template = 'template')
 	 {
-		 $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
 		 // use layout content if provided
 		 if (!isset($this->data['content']))
 			 $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
